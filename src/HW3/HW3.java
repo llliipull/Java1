77fb66ad0452dafc8cffde08734d2887db0ec5d8
llliipull <evil92.92@gmail.com>
2022-02-28 19:42:59 +0400

    HW3 1-5

---
+++ src/HW3/HW3.java
@@ -0,0 +1,84 @@
+package HW3;
+
+import java.util.Random;
+
+public class HW3 {
+
+    public static void main(String[] args){
+
+        int[] firstArray = new int[10];
+        System.out.println("Before");
+        for (int i = 0; i < 10; i++){
+            Random random = new Random();
+            firstArray[i] = random.nextInt(2);
+            System.out.print(firstArray[i] + " ");
+        }
+        System.out.println();
+        System.out.println("After");
+        for (int i = 0; i < 10; i++){
+
+            if (firstArray[i] == 1) {
+                firstArray[i] = 0;
+            } else{
+                firstArray[i] = 1;
+            }
+            System.out.print(firstArray[i] + " ");
+        }
+
+
+        System.out.println();
+        int[] secondArray = new int[100];
+
+        for (int i = 0; i < 100; i++){
+            secondArray[i] = i + 1;
+            System.out.print(secondArray[i] + " ");
+        }
+
+        System.out.println();
+        int[] thirdArray = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < 12; i++){
+            if (thirdArray[i] < 6){
+                thirdArray[i] = thirdArray[i] * 2;
+            }
+            System.out.print(thirdArray[i] + " ");
+        }
+
+
+        int[][] forthArray = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            System.out.println();
+            for (int j = 0; j < 3; j++) {
+                if ((i == 0 && j != 1) || (i == 1 && j == 1) || (i == 2 && j != 1)) {
+                    forthArray[i][j] = 1;
+                }
+                System.out.print(forthArray[i][j] + " ");
+                }
+            }
+
+        System.out.println();
+        printArray(fifthArray(3, 22));
+
+    }
+
+        public static void printArray(int[] array){
+            for (int i = 0; i < array.length; i++) {
+                System.out.print(array[i] + " ");
+            }
+        }
+
+        public static int[] fifthArray(int len, int initialValue){
+            int[] returnedArray = new int[len];
+            for (int i = 0; i < len; i++) {
+                returnedArray[i] = initialValue;
+            }
+
+            return returnedArray;
+
+        }
+
+
+
+}
+
+
+
